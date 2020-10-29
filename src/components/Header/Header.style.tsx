c65087636f231f2577c6f8ebc57df59f0f8c018d
capsuleman <guillaume.vagner@gmail.com>
2020-10-29 23:28:29 +0100

    🚧 added simple dashboard

---
+++ src/components/Header/Header.style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
+export const Title = styled(Typography)`
+  flex-grow: 1;
+  font-size: 1.2rem;
+`;
+
+export const Username = styled(Typography)`
+  margin-right: 12px;
+`;
