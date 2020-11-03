66fd2445407152e547f72733d37b1f31555980ff
capsuleman <guillaume.vagner@gmail.com>
2020-11-03 23:24:14 +0100

    🚧 added number of bets in get all options

--- functions/options-get-all.js
+++ functions/options-get-all.js
@@ -33,8 +33,65 @@ exports.handler = async (event, context) => {
       };
     }
 
-    const allOptionsResult = await client.query(q.Paginate(q.Match(q.Index('all_options'))));
-    const allOptions = allOptionsResult.data.map(([ref, name]) => ({ id: ref.id, name }));
+    const allOptionsResult = await client.query(
+      q.Let(
+        {
+          bets_value_name: q.Map(
+            q.Paginate(q.Documents(q.Collection('bets'))),
+            q.Lambda(['bet'], {
+              value: q.Select(['data', 'value'], q.Get(q.Var('bet'))),
+              option: q.Select(['data', 'option'], q.Get(q.Var('bet'))),
+            }),
+          ),
+          bets_sum_by_option: q.Map(
+            q.Paginate(q.Documents(q.Collection('options'))),
+            q.Lambda(
+              ['option'],
+              [
+                q.Select(['id'], q.Var('option')),
+                q.Select(
+                  [0],
+                  q.Sum(
+                    q.Map(
+                      q.Filter(
+                        q.Var('bets_value_name'),
+                        q.Lambda(
+                          ['bet'],
+                          q.Equals(
+                            q.Select(['id'], q.Var('option')),
+                            q.Select(['option', 'id'], q.Var('bet')),
+                          ),
+                        ),
+                      ),
+                      q.Lambda(['option_bet'], q.Select(['value'], q.Var('option_bet'))),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        },
+        q.Map(
+          q.Paginate(q.Match(q.Index('all_options'))),
+          q.Lambda('option', {
+            id: q.Select([0, 'id'], q.Var('option')),
+            name: q.Select([1], q.Var('option')),
+            numberOfBets: q.Select(
+              [0, 1],
+              q.Filter(
+                q.Var('bets_sum_by_option'),
+                q.Lambda(
+                  'bet',
+                  q.Equals(q.Select([0], q.Var('bet')), q.Select([0, 'id'], q.Var('option'))),
+                ),
+              ),
+            ),
+          }),
+        ),
+      ),
+    );
+
+    const allOptions = allOptionsResult.data;
 
     return {
       statusCode: 200,
